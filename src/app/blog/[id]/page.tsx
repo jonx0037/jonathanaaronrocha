ce2b1c038bfcde27bab4ecc39fbd5a50a336d5e0
Jonathan Rocha <jonx0037@gmail.com>
2024-10-14 11:22:00 -0500

    Update blog post page for static generation

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,18 +1,11 @@
-import { useEffect } from 'react';
 import { useDarkMode } from '../../../context/DarkModeContext';
-import DarkModeToggle from '../../components/DarkModeToggle';
 import { blogPosts } from '../../blogPosts';
 import styles from '../../page.module.css';
 import Link from 'next/link';
 
 export default function BlogPost({ params }: { params: { id: string } }) {
-  const { darkMode } = useDarkMode();
   const post = blogPosts.find(post => post.id === params.id);
   const otherPosts = blogPosts.filter(p => p.id !== params.id);
-
-  useEffect(() => {
-    document.body.className = darkMode ? 'dark-mode' : 'light-mode';
-  }, [darkMode]);
 
   if (!post) {
     return <div>Post not found</div>;
@@ -20,7 +13,6 @@ export default function BlogPost({ params }: { params: { id: string } }) {
 
   return (
     <div className={styles.blogPost}>
-      <DarkModeToggle />
       <h1>{post.title}</h1>
       <p>{post.date}</p>
       <div dangerouslySetInnerHTML={{ __html: post.content }} />
